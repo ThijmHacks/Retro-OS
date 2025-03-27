50bb4e13c9629182b2a3f15adc461ce055d3c999
ThijmHacks <148696012+ThijmHacks@users.noreply.github.com>
2025-03-27 22:35:57 +0100

    Created a logo, not final logo but begin of...

--- src/retro_os/booting/bootingfase.py
+++ src/retro_os/booting/bootingfase.py
@@ -1,15 +1,26 @@
 import tkinter as tk
+from tkinter import PhotoImage
 
 import retro_os.booting.fasechanger
 
 
 def bootloader(root):
+    root.config(background="black")
     label = tk.Label(root, text="Booting...", font=("Arial", 24))
-    label.pack()
+    label.pack(side='top', anchor="center")
     button = tk.Button(root, text="Load OS into disk", font=("Arial", 14),
                        command=lambda: retro_os.booting.fasechanger.switch_fase(root, "osloader"))
     button.pack()
 
+
+# noinspection PyTypeChecker
 def osloader(root):
+    root.config(background="lightgray")
     label = tk.Label(root, text="This is the OS Loader", font=("Arial", 24))
     label.pack()
+
+    oslogo = PhotoImage(file="~/Onedrive/Documenten/Github/ThijmHacks/Retro-OS/src/retro_os/booting/os-logo.png")
+
+    root.oslogo = oslogo
+    logolabel = tk.Label(root, image=oslogo)
+    logolabel.pack()
