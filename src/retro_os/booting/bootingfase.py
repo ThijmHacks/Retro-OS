bfcb46df288f9ded39cf6f0e25596a94b10f2de2
ThijmHacks <148696012+ThijmHacks@users.noreply.github.com>
2025-03-28 17:04:41 +0100

    Changed the logo a bit upwards.

--- src/retro_os/booting/bootingfase.py
+++ src/retro_os/booting/bootingfase.py
@@ -14,7 +14,6 @@ def bootloader(root):
     root.config(bg="black")
 
     laptoplogo_path = os.path.expanduser("~/Onedrive/Documenten/Github/ThijmHacks/Retro-OS/src/retro_os/booting/laptop-logo.png")
-
     laptoplogo = Image.open(laptoplogo_path)
     laptoplogo_small = laptoplogo
     width, height = laptoplogo_small.size
@@ -23,9 +22,11 @@ def bootloader(root):
     laptoplogo_resized = laptoplogo_small.resize((nwidth, nheight))
     laptoplogo = ImageTk.PhotoImage(laptoplogo_resized)
 
+
     root.laptoplogo = laptoplogo
     logolabel = tk.Label(root, image=laptoplogo, background="black")
-    logolabel.place(relx=0.5, rely=0.5, anchor="center")
+    logolabel.place(relx=0.5, rely=0.3, anchor="center")
+
 
     root.after(5000, lambda: bootloader_await_5s(root))
 
