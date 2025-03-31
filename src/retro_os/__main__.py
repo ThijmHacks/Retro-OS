fb7aa6fd01c4d203549f4b5e9adaad99e0d174d8
ThijmHacks <148696012+ThijmHacks@users.noreply.github.com>
2025-03-31 22:32:48 +0200

    Just small changes small changes to: - __main__.py - icon.ico

--- src/retro_os/__main__.py
+++ src/retro_os/__main__.py
@@ -1,13 +1,14 @@
 import tkinter as tk
+
 from retro_os.booting.fasechanger import switch_fase as sf
 from retro_os.login.fasechanger import switch_fase as sfn
 
-
-class RetroOS():
+class RetroOS:
     def __init__(self):
         root = tk.Tk()
         root.title("RetroOS")
         root.attributes("-fullscreen", True)
         #sf(root, "bootloader")
         sfn(root, "initializing_start")
+
         root.mainloop()
