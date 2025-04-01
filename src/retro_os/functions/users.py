bb09b4fd8de4c082df9e66245013c35ca2b8ce89
ThijmHacks <148696012+ThijmHacks@users.noreply.github.com>
2025-04-01 20:48:14 +0200

    fixed saving...

--- src/retro_os/functions/users.py
+++ src/retro_os/functions/users.py
@@ -1,10 +1,25 @@
 import pickle
+import os
 
 def create_user_file():
+    savedir = "retroos/users/"
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+
+
     file = open("retroos/users/user_list.ul", "w")
     file.close()
 
+
+
 def create_user(username, password):
+    try:
+        with open('retroos/users/user_list.ul', 'rb') as f:
+            users = pickle.load(f)
+    except (EOFError):
+        users = {}
+
+
     users[username] = password
 
     with open('retroos/users/user_list.ul', 'wb') as f:
